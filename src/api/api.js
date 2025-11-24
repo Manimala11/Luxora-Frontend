e10ccf664255347ffc28298bc58a1ffbbe248cd1
Manimala S <manimala3214@gmail.com>
2025-11-24 15:51:51 +0530

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL: "https://luxora-backend-lime.vercel.app/api"
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://luxora-backend-lime.vercel.app/api"
 })
 
 export default api;
